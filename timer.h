130b1ebc265da675c648c69fce4ae1c95a2885b2
smk12 <smk12@email.arizona.edu>
2015-10-19 14:36:45 -0700

    Create timer.h

---
+++ timer.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   timer.h
+ * Authors:
+ *
+ * Created on December 30, 2014, 8:07 PM
+ */
+
+#ifndef INITTIMER_H
+#define	INITTIMER_H
+
+void delayUs(unsigned int delay);
+void delayMs(unsigned int delay);
+void MsTMR1(unsigned int delay);
+
+
+
+#endif	/* INITTIMER_H */
+
