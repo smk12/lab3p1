f52cbb42e9a1599241ce5121d13c36936e176711
smk12 <smk12@email.arizona.edu>
2015-10-26 16:31:51 -0700

    Update adc.c

--- adc.c
+++ adc.c
@@ -9,21 +9,23 @@
 
 void initADC(){
     
-    ANSELBbits.ANSB0 = 0; 
+    TRISBbits.TRISB1 = 1;
+    ANSELBbits.ANSB1 = 0; // pin 33 j11
+    
     AD1CON1bits.FORM = 0; // 16 unsigned integer
     AD1CON1bits.SSRC = 7; // Auto-convert mode
     AD1CON1bits.ASAM = 1; // Auto-sampling
     AD1CON2bits.VCFG = 0; // Use board refernece voltages
     AD1CON2bits.CSCNA = 0; // Disable scanning
-    AD1CON2bits.SMPI = 15; // 15 burritos
+    AD1CON2bits.SMPI = 0; // 1 burrito
     AD1CON2bits.ALTS = 0; // Only Mux A
     AD1CON3bits.ADRC = 0; // Use PBCLK
     AD1CON3bits.SAMC = 2; // 2 Tad per sample
-    AD1CON3bits.ADCS = 0xFF; // 512 times the PBCLK
+    AD1CON3bits.ADCS = 1; // 4 times the PBCLK
     AD1CHSbits.CH0NA = 0; // Use Vref- as negative reference
-    AD1CHSbits.CH0SA = 0; // Scan AN0 at least
-    IFS0bits.AD1IF = 0;
-    IEC0bits.AD1IE = 1;
-    IPC5bits.AD1IP = 7;
+    AD1CHSbits.CH0SA = 1; // Scan AN0 at least
+    IFS0bits.AD1IF = 0; // Put down ADC flag
+    //IPC5bits.AD1IP = 7;
+    //IEC0bits.AD1IE = 1;
     AD1CON1bits.ADON = 1; // turn on the ADC
 }
